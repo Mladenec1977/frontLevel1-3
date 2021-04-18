c4497aef66fa21f44e8520f499f93d178bc45e3e
Mladenec1977 <rozgon1977@gmail.com>
2021-04-18 13:43:43 +0300

    JQuery add

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-$(function(){
+$(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        autoplay: 1000,
+        navigation: true, // Show next and prev buttons
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        singleItem: true,
+        loop: true,
+        nav: true
 
+        // "singleItem:true" is a shortcut for:
+        // items : 1,
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+    });
 });
